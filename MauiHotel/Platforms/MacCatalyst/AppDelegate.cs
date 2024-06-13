f19e35675c3c8069c8fea5628a29a348d2839611
Usuario <Usuario@CPD03-M20>
2024-06-13 13:52:22 -0300

    Adicionar arquivos de projeto.

---
+++ MauiHotel/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiHotel
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ MauiHotel/Quartos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiHotel
+{
+    public class Quartos
+    {
+        public string Descricao { get; set; }
+        public
+    }
+}
